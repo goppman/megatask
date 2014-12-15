b1bffd80a8365aa796377e704480a017c539d6c0
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-15 15:36:50 -0500

    saved local and wrote local information

--- js/megatask.js
+++ js/megatask.js
@@ -2,33 +2,48 @@ var Megatask = function () {
   function Megatask () {
     this.tasks =[];
     var self = this;
+
     var supportsStorage = function () {
       try {
         return 'localStorage' in window && window['localStorage'] != null;
       }catch(e){
         return false;
       }
-
     };
 
     var loadTasks = function () {
-      if (supportsStorage) {
+      if (supportsStorage() && localStorage.tasks) {
        self.tasks=JSON.parse(localStorage.tasks);
-      }
-    }
+       for (i=0; i < self.tasks.length; i++) {
+         var taskToAppend = self.tasks[i];
+         $('#tasks').append(createListItem(taskToAppend));
 
-    var addTask = function (taskName) {
-      self.tasks.push(taskName);
-      $('#tasks').append('<li class"list-group-item">' +taskName +'</li>');
+       }
+      }
+    };
+
+    var createListItem = function (task) {
+      return '<li class="list-group-item">'+ task.name + '</li>'
+
+
+    };
+
+    var addTask = function (taskName, taskCompleted) {
+      taskCompleted = !!taskCompleted;
+      var newTask = {
+        name: taskName,
+        completed: taskCompleted
+      };
+      self.tasks.push(newTask);
+      $('#tasks').append('<li class="list-group-item">' + taskName +'</li>');
       saveTasks();
     };
 
     var saveTasks = function() {
-      if (supportsStorage){
+      if (supportsStorage() && localStorage.tasks){
         localStorage.tasks = JSON.stringify(self.tasks);
       }
     };
-
 
     $('#new_task').submit(function (ev) {
       ev.preventDefault();
@@ -36,7 +51,6 @@ var Megatask = function () {
       addTask(field.val());
       field.val('');
       //this.elements.task_name.value
-
     });
 
     loadTasks();
