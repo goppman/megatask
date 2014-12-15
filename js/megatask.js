603d578c104ed796339019eb8c4c6e699a6acbdb
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-15 18:20:58 -0500

    added task delete functionality

--- js/megatask.js
+++ js/megatask.js
@@ -1,6 +1,7 @@
 var Megatask = function () {
   function Megatask () {
     this.tasks =[];
+    this.counter=0;
     var self = this;
 
     var supportsStorage = function () {
@@ -23,24 +24,33 @@ var Megatask = function () {
     };
 
     var createListItem = function (task) {
-      return '<li class="list-group-item">'+ task.name + '</li>'
+      var deleteButton, editButton, buttonGroup, label;
+      label = '<label> '+ task.name + '</label>';
+      deleteButton = '<button  class="btn btn-sm btn-danger" ><i class=" fa fa-trash-o fa-lg"></i></button>';
+      editButton = '<button class="btn btn-sm btn-success">edit</button>';
+      buttonGroup = '<div class="btn-group" >' + deleteButton +
+      editButton + '</div>';
+      return '<li class="list-group-item" id="task_' + task.id + '"><div class="task">'+
+      label + buttonGroup +'</div></li>';
 
 
     };
 
     var addTask = function (taskName, taskCompleted) {
       taskCompleted = !!taskCompleted;
+      self.counter++;
       var newTask = {
+        id: self.counter,
         name: taskName,
         completed: taskCompleted
       };
       self.tasks.push(newTask);
-      $('#tasks').append('<li class="list-group-item">' + taskName +'</li>');
+      $('#tasks').append(createListItem(newTask));
       saveTasks();
     };
 
     var saveTasks = function() {
-      if (supportsStorage() && localStorage.tasks){
+      if (supportsStorage() ){
         localStorage.tasks = JSON.stringify(self.tasks);
       }
     };
@@ -52,6 +62,19 @@ var Megatask = function () {
       field.val('');
       //this.elements.task_name.value
     });
+    $('#tasks').on('click','button.btn-danger', function(){
+      var listItem = $(this).closest('li');
+      var id = listItem.attr('id');
+      id = id.substring(id.lastIndexOf('_')+1)
+      for(var i=0; i < self.tasks.length; i++) {
+        if (self.tasks[i].id.toString()===id) {
+          self.tasks.splice(i, 1);
+        }
+      }
+      listItem.remove();
+      saveTasks();
+
+    })
 
     loadTasks();
 
